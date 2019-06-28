a78bf10a698f49218042b6fddc6385e8b0617e6f
pkpkpkpkpkpkpkpk <pk.2121@gmail.com>
2019-06-28 21:22:16 +1000

    set up dev environment

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './styles';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>...</Text>
+    </View>
+  );
+}
+
+export default App;
